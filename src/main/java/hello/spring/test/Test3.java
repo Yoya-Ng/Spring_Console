23283ebd7acb45db4677b50c4ce0274e2d309847
Yoya <unrealboundary@gmail.com>
2019-06-14 21:39:20 +0800

    Fastfood，建立 List & Set & Map 參數

--- src/main/java/hello/spring/test/Test3.java
+++ src/main/java/hello/spring/test/Test3.java
@@ -1,16 +1,19 @@
 package hello.spring.test;
 
+import hello.spring.beans.IFastfood;
+import hello.spring.beans.IHelloSpring;
 import hello.spring.beans.ILottoSpring;
+import hello.spring.beans.LottoSpring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-public class LottoText {
-
+public class Test3 {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ILottoSpring lotto = (ILottoSpring)context.getBean("lotto5");
-        //ILottoSpring lotto2 = (ILottoSpring)context.getBean("lotto2");
-        lotto.print();
-        //lotto2.print();
-    }
+        IFastfood fastfood = (IFastfood)context.getBean("fastfood4");
+        fastfood.print();
+        
+    }    
 }
+
