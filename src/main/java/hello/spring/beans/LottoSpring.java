439c7aaf6f06cf25ad793f2137d30fd15874c1eb
Yoya <unrealboundary@gmail.com>
2019-06-14 21:00:33 +0800

    Spring 設定檔 放入多個值

--- src/main/java/hello/spring/beans/LottoSpring.java
+++ src/main/java/hello/spring/beans/LottoSpring.java
@@ -1,12 +1,10 @@
 package hello.spring.beans;
 
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
 
-public class LottoSpring implements ILottoSpring {
-
+public class LottoSpring implements ILottoSpring{
     private int count;
 
     public LottoSpring() {
@@ -15,19 +13,24 @@ public class LottoSpring implements ILottoSpring {
     public LottoSpring(int count) {
         this.count = count;
     }
+    
+    public LottoSpring(int a, int b) {
+        this.count = (int)Math.pow(a, b);
+    }
+    
 
     public void setCount(int count) {
         this.count = count;
     }
-
+    
     @Override
     public void print() {
-        // 取出 count 個不重複的數字，利用 Ｓｅｔ
+        // 取出 count 個不重複數字, 利用 Set
         Set<Integer> nums = new LinkedHashSet<>();
-        while (nums.size() <= count) {
+        while (nums.size() < count) {            
             nums.add(new Random().nextInt(100));
         }
         System.out.println(nums);
     }
-
+    
 }
