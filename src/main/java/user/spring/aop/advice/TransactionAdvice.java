afad82841b17381bb59cdb5b014502ada1537add
Yoya <unrealboundary@gmail.com>
2019-06-21 20:41:31 +0800

    Spring AOP > before & after

---
+++ src/main/java/user/spring/aop/advice/TransactionAdvice.java
@@ -0,0 +1,16 @@
+package user.spring.aop.advice;
+
+import org.aspectj.lang.JoinPoint;
+
+public class TransactionAdvice {
+    public void before(JoinPoint joinPoint) {
+        if(joinPoint.getArgs() != null && joinPoint.getArgs().length > 0) {
+            System.out.println("前置通知被執行:" + joinPoint.getArgs()[0]);
+        } else {
+            System.out.println("前置通知被執行");
+        }
+    }
+    public void after(JoinPoint joinPoint) {
+        System.out.println("後置通知被執行");
+    }
+}
