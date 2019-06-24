21b6b92e746fa29e36e159a7ea599a61275b6d71
Yoya <unrealboundary@gmail.com>
2019-06-24 20:46:10 +0800

    題目

---
+++ src/main/java/book/jdbc/test/CRUD_2.java
@@ -0,0 +1,17 @@
+package book.jdbc.test;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import javax.activation.DataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class CRUD_2 {
+
+    public static void main(String[] args) {
+
+// 如何裡用 Brans 完成
+        System.out.println(dataSource.getJdbcUrl());
+        
+    }
+
+}
