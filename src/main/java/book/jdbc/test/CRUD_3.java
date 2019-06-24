719784b5e88052189748c1720a56c05e78d295d9
Yoya <unrealboundary@gmail.com>
2019-06-24 21:45:14 +0800

    自動導入 new BeanPropertyRowMapper(Book.class)

--- src/main/java/book/jdbc/test/CRUD_3.java
+++ src/main/java/book/jdbc/test/CRUD_3.java
@@ -13,6 +13,7 @@ public class CRUD_3 {
         ApplicationContext context 
                 = new ClassPathXmlApplicationContext("applicationContext_jdbc.xml"); 
         dao = (IBookDaoMapper) context.getBean("bookDaoMapper"); 
+        System.out.println(get()); 
         read(); 
     } 
      
@@ -20,6 +21,10 @@ public class CRUD_3 {
         List<Book> list = dao.query(); 
         out.println(list.size()); 
         out.println(list); 
+    } 
+     
+    public static Book get() { 
+        return dao.get(3); 
     } 
      
 } 
